53412dd43cd57ca61490d87ea3cb9fed31d26891
bhanu_mittal <bhanu_mittal@apple.com>
2018-06-01 14:35:14 +0530

    final sample

--- forwardprop.py
+++ forwardprop.py
@@ -28,7 +28,7 @@ def forward(X, W1, b1, W2, b2):
     Z = 1/(1 + np.exp(-X.dot(W1) - b1))
     A = Z.dot(W2) + b2
     expA = np.exp(A)
-    Y = expA.sum(axis=1,keepdims = True)
+    Y = expA/expA.sum(axis=1,keepdims = True)
     return Y
 
 def classification_rate(Y,P):
