97c4c83a835dcda15cd16b81bcc81471f99a147a
Wrekkers <bhanu93.iitd@gmail.com>
2018-06-09 22:47:47 +0530

    added kaggle facial recognition

--- facial_reg.py
+++ facial_reg.py
@@ -29,40 +29,83 @@ def init_weights(shape):
 def neural_net(X, Wx, Wh, Wy, bh, by):
     
     layers = Wh.shape[0]
-    
+
+    Z1 = tf.nn.relu(tf.matmul(X, Wx) + bh)
+
+    #Z2 = tf.nn.relu(tf.matmul(Z1, Wh[0]) + bh[1])
+
+    #Z3 = tf.nn.relu(tf.matmul(Z2, Wh[1]) + bh[2])
+
+
+    """
     last_layer = tf.nn.relu((tf.matmul(X,Wx) + bh[0]))
     
     
     for i in range(layers-1):
-        
-    
-    raise NotImplementedError
+        Z = tf.nn.relu(tf.matmul(last_layer,Wh[i]) + bh[i+1])
+        last_layer = Z
+    """
+    Y = tf.matmul(Z1,Wy) + by
+    return Y
+
+
 
 def main():
-    #X, Y, X_test, Y_test = get_data('../fer2013.csv')
+    X, Y, X_test, Y_test = get_data('../fer2013.csv')
      
-    #D = X.shape[1]  # size of the input layer
-    D = 48 * 48
+    D = X.shape[1]  # size of the input layer
+    print('Done reading data')
+    print(D)
+    #D = 48 * 48
     M = 100          # size of hidden layers
     K = 7           # no. of classes
-    layers = 10     # no. of hidden layers
+    layers = 2     # no. of hidden layers
     
     expression = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy',
                      4:'Sad', 5:'Surprise', 6:'Neutral'}
+    
+    T = one_hot(Y, K)
     
     tfX = tf.placeholder(tf.float32, [None, D])
     tfY = tf.placeholder(tf.float32, [None, K])
     
     Wx = tf.Variable(init_weights([D,M]))
-    bh = tf.Variable(init_weights([layers-1,M]))
-    Wh = tf.Variable(init_weights([layers-1,M,M]))
+    #bh = tf.Variable(init_weights([layers,M]))
+    bh = tf.Variable(init_weights([M]))
+    Wh = tf.Variable(init_weights([M,M]))
+    #Wh = tf.Variable(init_weights([layers-1,M,M]))
     Wy = tf.Variable(init_weights([M,K]))
     by = tf.Variable(init_weights([K]))
     
     
+    y_out = neural_net(tfX, Wx, Wh, Wy, bh, by)
     
-    print(W[0])
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = y_out, labels = tfY))
+        
+    train_op = tf.train.GradientDescentOptimizer(3*10e-7).minimize(cost)
     
+    predict_op = tf.argmax(y_out, axis=1)
+    
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    
+    score_old = 0
+    score = 1
+    for i in range(10000):
+        sess.run(train_op, feed_dict = {tfX: X, tfY : T})
+        pred = sess.run(predict_op, feed_dict = {tfX: X, tfY : T})
+        
+        if(i % 10 == 0):
+            score = np.mean(pred == Y)
+            print('Iteration: ' + str(i) + ', Score: ' + str(score))
+            
+        if (i % 1000 == 0):
+            if (score < score_old):
+                break
+            else:
+                score_old = score
+
 
 if __name__ == '__main__':
     main()
